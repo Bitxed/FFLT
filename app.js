8ad5f1ffbfbfb3a0a9dac475cd1c2b1bf4bf3ee4
Bitxed <contact@bitxed.com>
2022-06-06 20:25:19 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,88 @@
+//Countdown Timer
+const clockdiv = document.getElementById("countdown");
+const countDownTime = new Date(
+  clockdiv.getAttribute("data-date")
+).getTime();
+
+const countdownfunction = setInterval(function () {
+  const now = new Date().getTime();
+  const diff = countDownTime - now;
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(diff % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+  const minutes = Math.floor(diff % (1000 * 60 * 60) / (1000 * 60));
+  const seconds = Math.floor(diff % (1000 * 60) / 1000);
+
+  if (diff < 0) {
+    clockdiv.style.display = "none";
+    clearInterval(countdownfunction);
+  } else {
+    clockdiv.querySelector(".days").innerHTML = days;
+    clockdiv.querySelector(".hours").innerHTML = hours;
+    clockdiv.querySelector(".minutes").innerHTML = minutes;
+    clockdiv.querySelector(".seconds").innerHTML = seconds;
+  }
+}, 1000);
+
+
+// METAMASK CONNECTION
+window.addEventListener('DOMContentLoaded', () => {
+  const onboarding = new MetaMaskOnboarding();
+  const onboardButton = document.getElementById('connectWallet');
+  let accounts;
+
+  const updateButton = () => {
+    if (!MetaMaskOnboarding.isMetaMaskInstalled()) {
+      onboardButton.innerText = 'Install MetaMask!';
+      onboardButton.onclick = () => {
+        onboardButton.innerText = 'Connecting...';
+        onboardButton.disabled = true;
+        onboarding.startOnboarding();
+      };
+    } else if (accounts && accounts.length > 0) {
+      onboardButton.innerText = `✔ ...${accounts[0].slice(-4)}`;
+      onboardButton.disabled = true;
+      onboarding.stopOnboarding();
+    } else {
+      onboardButton.innerText = 'Connect MetaMask!';
+      onboardButton.onclick = async () => {
+        await window.ethereum.request({
+          method: 'eth_requestAccounts',
+        })
+        .then(function(accounts) {
+          onboardButton.innerText = `✔ ...${accounts[0].slice(-4)}`;
+          onboardButton.disabled = true;
+        });
+      };
+    }
+  };
+
+  updateButton();
+  if (MetaMaskOnboarding.isMetaMaskInstalled()) {
+    window.ethereum.on('accountsChanged', (newAccounts) => {
+      accounts = newAccounts;
+      updateButton();
+    });
+  }
+});
+
+function myFunction() {
+  // Get the snackbar DIV
+  var x = document.getElementById("snackbar");
+
+  // Add the "show" class to DIV
+  x.className = "show";
+
+  // After 3 seconds, remove the show class from DIV
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
+
+function myFunction1() {
+  // Get the snackbar DIV
+  var x = document.getElementById("MerchStore");
+
+  // Add the "show" class to DIV
+  x.className = "show";
+
+  // After 3 seconds, remove the show class from DIV
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
